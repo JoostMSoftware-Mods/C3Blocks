1e30e6c5057abf38070ca65830cd45ce391bcffb
Joost <joostmsoftware@gmail.com>
2023-08-03 10:15:39 +0200

    1.20.x support added!

--- src/main/java/com/joostmsoftware/c3blocks/client/C3Client.java
+++ src/main/java/com/joostmsoftware/c3blocks/client/C3Client.java
@@ -8,22 +8,28 @@ import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.text.Text;
 
 import java.util.List;
 
 public class C3Client implements ClientModInitializer {
 
     private static final List<Block> blockList = C3Util.getBlockEntries();
+    private static final RegistryKey<ItemGroup> ITEM_GROUP = RegistryKey.of(RegistryKeys.ITEM_GROUP, C3.ID("group"));
 
     @Override
     public void onInitializeClient() {
         C3ClientRegistry.registerClient();
         RRPCallback.BEFORE_VANILLA.register(resources -> resources.add(C3.RESOURCE_PACK));
 
-        ItemGroup GROUP = FabricItemGroup.builder(C3.ID("group"))
-                .icon(() -> new ItemStack(blockList.get(2)))
-                .entries((displayContext, entries) -> blockList.forEach(block -> entries.add(block.asItem())))
-                .build();
+        Registry.register(Registries.ITEM_GROUP, ITEM_GROUP, FabricItemGroup.builder()
+                .displayName(Text.translatable("itemGroup.c3blocks.group"))
+                .icon(() -> new ItemStack(blockList.get(0)))
+                .entries((context, entries) -> blockList.forEach(block -> entries.add(block.asItem()))).build());
 
         C3.LOGGER.info("Loaded all client entries!");
     }
