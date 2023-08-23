549f6c1811a61ac4384969ad4fa417e81ca34a75
Joost <joostmsoftware@gmail.com>
2023-08-23 08:57:00 +0200

    v1.1.0

--- src/main/java/com/joostmsoftware/c3blocks/client/C3Client.java
+++ src/main/java/com/joostmsoftware/c3blocks/client/C3Client.java
@@ -2,12 +2,12 @@ package com.joostmsoftware.c3blocks.client;
 
 import com.joostmsoftware.c3blocks.C3;
 import com.joostmsoftware.c3blocks.util.C3Util;
-import net.devtech.arrp.api.RRPCallback;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import pers.solid.brrp.v1.fabric.api.RRPCallback;
 
 import java.util.List;
 
@@ -18,7 +18,7 @@ public class C3Client implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         C3ClientRegistry.registerClient();
-        RRPCallback.BEFORE_VANILLA.register(resources -> resources.add(C3.RESOURCE_PACK));
+        RRPCallback.BEFORE_VANILLA.register(resources -> resources.add(C3.PACK));
 
         ItemGroup GROUP = FabricItemGroup.builder(C3.ID("group"))
                 .icon(() -> new ItemStack(blockList.get(2)))
